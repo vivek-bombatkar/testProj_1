f1a0123700ba70a0eb3e2add630de60c5f22b07a
Vivek <bombatkar.vivek@gmail.com>
2017-11-07 21:18:52 +0100

    Initial commit

---
+++ scr/functions.py
@@ -0,0 +1,9 @@
+import string
+
+def myName(name):
+    return "my name is " + name
+
+def myAge(age):
+    return str(age) + " is my age "
+
+
---
+++ scr/helloWorld.py
@@ -0,0 +1,12 @@
+import functions as py
+
+
+
+def funTest(name):
+    return  "hello " + name
+
+print(funTest("vivek"))
+
+print(py.myAge(31))
+
+print(py.myName("VIVEK"))
