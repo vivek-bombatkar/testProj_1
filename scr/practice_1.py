61b15757208dcb7f27541a8a2340391e13dacacf
Vivek <bombatkar.vivek@gmail.com>
2017-11-12 18:46:08 +0100

    done

--- scr/practice_1.py
+++ scr/practice_1.py
@@ -16,16 +16,6 @@ frmt=lg.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 hdlr.setFormatter(frmt)
 lgr.addHandler(hdlr)
 
-PATH= r'C:\VIVEK\PROJECTS\practice_1\working'
-URL = "https://www.udemy.com/gcp-data-engineer-and-cloud-architect/"
-if len(sys.argv) == 3:
-    URL=sys.argv[1]
-    PATH=sys.argv[2]
-    lgr.info("URL : " + URL)
-    lgr.info("PATH : " + PATH)
-
-PATH= PATH + "\data_" + time.strftime('%d%m%Y_%H%M%S') + ".txt"
-
 def getRequests(URL):
     try:
         lgr.info("calling page : " + URL)
@@ -42,12 +32,12 @@ def getUrllib2(URL):
 def getFile(path):
     return open(path,mode='a')
 
-def exeSQL(sql,v1,v2):
+def exeSQL(sql,*values):
     try:
-        lgr.info(sql,v1,v2)
+        #lgr.info(sql,values)
         con=cntr.connect(host='localhost',database='sakila',user='root',password='root')
         cur=con.cursor()
-        cur.execute(sql,(v1,v2))
+        cur.execute(sql,values)
         con.commit()
     except:
         lgr.error(sys.exc_info()[0])
@@ -63,21 +53,42 @@ def getURLS(data):
     url=data[startQ+1:endQ]
     return url,endQ
 
-def main():
+def processURL(timestampID,URL,PATH):
+    #timestampID=time.strftime('%d%m%Y%H%M%S')
     urlText=getRequests(URL)
+    #getFile(PATH).write(urlText)
     if urlText <> "":
         while True:
-            url,n  = getURLS(urlText)
+            nextURL,n  = getURLS(urlText)
             urlText = urlText[n:]
-            if url:
-                if "https://" in url:
-                    getFile(PATH).write(url+'\n')
+            if nextURL:
+                if "https://" in nextURL:
+                    nextURL=nextURL.replace("/url?q=","")+'\n'
+                    getFile(PATH).write(nextURL)
+                    lgr.info(exeSQL(
+                        "insert into parent_child_urls (idparent_child_urls,parent_url,child_url) "
+                        "values (%s,%s,%s)", timestampID, URL,nextURL))
+                    #processURL(timestampID, nextURL, PATH)
             else:
                 break
-        #"insert into batch_test (id,URL) values ('{0}','{1}')".format(time.strftime('%H%M%S'),"YYY"
-        lgr.info(exeSQL("insert into batch_test (id,URL) values (%s,%s)",time.strftime('%H%M%S'),URL))
+            #"insert into batch_test (id,URL) values ('{0}','{1}')".format(time.strftime('%H%M%S'),"YYY"
+        lgr.info(exeSQL("insert into batch_test (id,URL) values (%s,%s)",timestampID,URL))
     else:
         lgr.info("NOTHING HAPPENED")
+    return None
+
+def main():
+    PATH = r'C:\VIVEK\PROJECTS\practice_1\working'
+    URL = "https://www.digitalocean.com/community/tutorials/an-introduction-to-string-functions-in-python-3"
+    if len(sys.argv) == 3:
+        URL = sys.argv[1]
+        PATH = sys.argv[2]
+        lgr.info("URL : " + URL)
+        lgr.info("PATH : " + PATH)
+    PATH = PATH + "\data_" + time.strftime('%d%m%Y_%H%M%S') + ".txt"
+
+    timestampID = time.strftime('%d%m%Y%H%M%S')
+    processURL(timestampID,URL,PATH)
 
 if __name__ == '__main__':
     main()
