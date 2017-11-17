42e0eb42f24b0d35e836315e7006472ea25f17a7
Vivek <bombatkar.vivek@gmail.com>
2017-11-17 21:40:49 +0100

    good

--- scr/facebook_1.py
+++ scr/facebook_1.py
@@ -1,20 +1,52 @@
 import os
 import json
 import urllib
-import pprint
-#import facebook
+import config
+import facebook
 
-ACCESS_TOKEN = 'XXX'
+
 
 host = "https://graph.facebook.com"
 path = "/me"
-params = urllib.urlencode({"access_token": ACCESS_TOKEN})
 
-url = "{host}{path}?{params}".format(host=host, path=path, params=params)
+def printMe():
+    #1
+    params = urllib.urlencode({"access_token": config.ACCESS_TOKEN_FB})
+    #2
+    url = "{host}{path}?{params}".format(host=host, path=path, params=params)
+    #3
+    resp = urllib.urlopen(url).read()
+    #4
+    me = json.loads(resp)
+    #print(me)
 
-resp = urllib.urlopen(url).read()
+def main():
+  # Fill in the values noted in previous steps here
+  cfg = {
+    "page_id"      : config.APP_ID_FB,  # Step 1
+    "access_token" : config.ACCESS_TOKEN_FB   # Step 3
+    }
 
-me = json.loads(resp)
+  api = get_api(cfg)
+  msg = "Hello, world!"
+  status = api.put_wall_post(msg)
 
+def get_api(cfg):
+  graph = facebook.GraphAPI(cfg['access_token'])
+  # Get page token to post as the page. You can skip
+  # the following if you want to post as yourself.
+  resp = graph.get_object('me/accounts')
+  page_access_token = None
+  for page in resp['data']:
+    if page['id'] == cfg['page_id']:
+      page_access_token = page['access_token']
+  graph = facebook.GraphAPI(page_access_token)
+  return graph
+  # You can also skip the above if you get a page token:
+  # http://stackoverflow.com/questions/8231877/facebook-access-token-for-pages
+  # and make that long-lived token as in Step 3
 
-print(me)
+if __name__ == "__main__":
+  main()
+#    assert self.access_token, "Write operations require an access token"
+#AssertionError: Write operations require an access token
