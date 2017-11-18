56baf297abd7450a72be4ac19925528e5683fccf
Vivek <bombatkar.vivek@gmail.com>
2017-11-18 11:01:28 +0100

    good

--- scr/twiter_2.py
+++ scr/twiter_2.py
@@ -1,15 +1,30 @@
 import sys, json, config
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from tweepy import OAuthHandler
+import tweepy
 
 def getTwitStream(strSearch):
     auth = OAuth(config.ACCESS_TOKEN, config.ACCESS_SECRET, config.CONSUMER_KEY, config.CONSUMER_SECRET)
     twitStram = TwitterStream(auth=auth)
     return twitStram.statuses.filter(track=strSearch, language="en")
 
+def getTwipy():
+    auth = OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return tweepy.Cursor(api.home_timeline).items()
+
 def main():
-    result = getTwitStream("clinikum nord nuremberg")
+    result = getTwitStream("nuremberg")
+    tweet_count = 10
     for twit in result:
+        tweet_count -= 1
         print twit["text"]
+        if tweet_count <= 0:
+            break
+    result = getTwipy()
+    for s in result:
+        print s.text
 
 if __name__ == '__main__':
     main()
