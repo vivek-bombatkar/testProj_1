b61087b4c237055dd205d5822d6f0c66b37831a1
Vivek <bombatkar.vivek@gmail.com>
2017-11-21 21:28:23 +0100

    good

---
+++ ML/textBlob_1.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+
+print TextBlob("i am happy").sentiment.polarity
+print TextBlob("i am happy").sentiment
+print TextBlob("i am sad").sentiment
+print TextBlob("i am nutral").sentiment
+
+print TextBlob("ich bin vivek").detect_language().capitalize()
+print TextBlob("i am rebul").correct().translate(to="hi")
+
+print TextBlob("i am happy and sad").tags
