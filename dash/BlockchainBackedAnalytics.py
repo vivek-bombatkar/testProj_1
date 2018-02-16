3702c0a609579d96f63fda6bb82dedc432f8b1db
Vivek <bombatkar.vivek@gmail.com>
2018-02-17 00:44:01 +0100

    good

--- dash/BlockchainBackedAnalytics.py
+++ dash/BlockchainBackedAnalytics.py
@@ -3,28 +3,18 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, State, Output, Event
-import time
-from textblob import TextBlob
-import dash_table_experiments as dt
-import pandas as pd
-import io
-import base64
-import dash_table_experiments as dt
 import blockChain as blockChain
-
-import datetime as date
-import hashlib as hash
 
 inputFileHash=''
 dataScienceModelHash=''
 resultDataHash=''
 
 input_data_set = ''
-data_science_module = ''
+data_science_model = ''
 result_data_set = ''
 
 n_clicks_data_set_cnt=0
-n_clicks_module_cnt=0
+n_clicks_model_cnt=0
 ##### THE DASH PAGE START
 app = dash.Dash()
 app.scripts.config.serve_locally = True
@@ -33,7 +23,7 @@ app.config['suppress_callback_exceptions']=True
 
 all_options = {
     'Data': [''],
-    'Module': [''],
+    'model': [''],
     'Result': ['']
 }
 
@@ -95,9 +85,9 @@ app.layout = html.Div(children=[
             children=[
                 html.H3('Model'),
                 dcc.Upload(
-                    id='upload_data_science_module',
+                    id='upload_data_science_model',
                     children=html.Div([
-                        'Data_Science_Module - Drag and Drop or ',
+                        'Data_Science_model - Drag and Drop or ',
                         html.A('Select a File')
                     ]),
                     style={
@@ -112,9 +102,9 @@ app.layout = html.Div(children=[
                     }
                 ),
                 html.Div([dcc.Textarea(id='train_data_tx_id', placeholder='Enter Blockchain Transaction ID for Train data...',value='', rows=1, style={'width': '50%','margin': '10px'}) ]),
-                html.Div(id='data_science_module'),
-                html.Div(id='file_hash_module'),
-                html.Button(id='Button_module', children='Add to blockchain', n_clicks=0),
+                html.Div(id='data_science_model'),
+                html.Div(id='file_hash_model'),
+                html.Button(id='Button_model', children='Add to blockchain', n_clicks=0),
                 html.Div(id=' '),
             ]
         ),
@@ -180,21 +170,21 @@ for tab in ['DataBlock', 'ModelBlock', 'ResultBlock']:
 #show file hash start#
 @app.callback(Output('file_hash_data_set', 'children'),
               [Input('upload_input_data_set', 'filename'),Input('upload_input_data_set', 'contents')])
-def upload_data_science_module(filename,contents):
+def upload_data_science_model(filename,contents):
     global inputFileHash
     inputFileHash = hash.sha256(contents.encode('ascii')).hexdigest()
     return html.Div([ html.H6(inputFileHash)])
 
-@app.callback(Output('file_hash_module', 'children'),
-              [Input('upload_data_science_module', 'filename'),Input('upload_data_science_module', 'contents')])
-def upload_data_science_module(filename,contents):
+@app.callback(Output('file_hash_model', 'children'),
+              [Input('upload_data_science_model', 'filename'),Input('upload_data_science_model', 'contents')])
+def upload_data_science_model(filename,contents):
     global dataScienceModelHash
     dataScienceModelHash = hash.sha256(contents.encode('ascii')).hexdigest()
     return html.Div([html.H6(dataScienceModelHash)])
 
 @app.callback(Output('file_hash_result_data_set', 'children'),
               [Input('upload_result_data_set', 'filename'),Input('upload_result_data_set', 'contents')])
-def upload_data_science_module(filename,contents):
+def upload_data_science_model(filename,contents):
     global resultDataHash
     resultDataHash = hash.sha256(contents.encode('ascii')).hexdigest()
     return html.Div([html.H6(resultDataHash)])
@@ -207,16 +197,16 @@ def upload_input_data_set(filename, contents):
     input_data_set = contents
     return html.Div([html.H6(filename)])
 
-@app.callback(Output('data_science_module', 'children'),
-              [Input('upload_data_science_module', 'filename'),Input('upload_data_science_module', 'contents')])
-def upload_data_science_module(filename,contents):
-    global data_science_module
-    data_science_module = contents
+@app.callback(Output('data_science_model', 'children'),
+              [Input('upload_data_science_model', 'filename'),Input('upload_data_science_model', 'contents')])
+def upload_data_science_model(filename,contents):
+    global data_science_model
+    data_science_model = contents
     return html.Div([html.H6(filename)])
 
 @app.callback(Output('result_data_set', 'children'),
               [Input('upload_result_data_set', 'filename'),Input('upload_result_data_set', 'contents')])
-def upload_data_science_module(filename,contents):
+def upload_data_science_model(filename,contents):
     global result_data_set
     result_data_set = contents
     return html.Div([html.H6(filename)])
@@ -224,18 +214,18 @@ def upload_data_science_module(filename,contents):
 #add to blockchain
 @app.callback(
     Output('output_bc', 'children'),
-    [Input('button_data_set', 'n_clicks'), Input('Button_module', 'n_clicks'), Input('Button_result_data_set', 'n_clicks')],
+    [Input('button_data_set', 'n_clicks'), Input('Button_model', 'n_clicks'), Input('Button_result_data_set', 'n_clicks')],
     [State('train_data_tx_id', 'value'),State('model_tx_id', 'value'),State('input_data_tx_id', 'value')])
-def update_output(n_clicks_data_set,n_clicks_module,n_clicks_result_data_set,value_train_data_tx_id,value_model_tx_id,value_input_data_tx_id):
+def update_output(n_clicks_data_set,n_clicks_model,n_clicks_result_data_set,value_train_data_tx_id,value_model_tx_id,value_input_data_tx_id):
     global n_clicks_data_set_cnt
-    global n_clicks_module_cnt
+    global n_clicks_model_cnt
 
-    if n_clicks_data_set == 0 and n_clicks_module == 0 and n_clicks_result_data_set == 0:
+    if n_clicks_data_set == 0 and n_clicks_model == 0 and n_clicks_result_data_set == 0:
         return ''
     prev_block = bc[len(bc)-1]
     if n_clicks_data_set_cnt <> n_clicks_data_set:
         data = "{{ hash_data: {} }}".format(inputFileHash)
-    elif n_clicks_module_cnt <> n_clicks_module:
+    elif n_clicks_model_cnt <> n_clicks_model:
         data = "{{ hash_model: {}, Index_training_data: {} }}".format(dataScienceModelHash,value_train_data_tx_id)
     else:
         data = "{{ hash_result_data: {}, Index_model: {}, Index_input_data: {} }}".format(resultDataHash,value_model_tx_id,value_input_data_tx_id )
@@ -243,7 +233,7 @@ def update_output(n_clicks_data_set,n_clicks_module,n_clicks_result_data_set,val
     new_block = blockChain.getNextBlock(prev_block, data)
     bc.append(new_block)
     n_clicks_data_set_cnt = n_clicks_data_set
-    n_clicks_module_cnt = n_clicks_module
+    n_clicks_model_cnt = n_clicks_model
     return html.Div([
 #        html.Div([dcc.Textarea(value="Block added to index {} ".format(str(bc[len(bc)-1].index)) ,id='dynamic-output',style={'width': '100%'})]),
 #        html.Div([html.H6("Index : Timestamp, Data", style={'width': '100%'})]),
